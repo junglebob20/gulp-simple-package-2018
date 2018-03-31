820f421514a631b9308e403ecb98bce39e8fef61
junglebob <vedut.artem20@gmail.com>
2018-03-31 03:50:45 +0200

    init package

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var validator = require('gulp-html');
+var uglify = require('gulp-uglify')
 var bs = require('browser-sync').create();
 
 gulp.task('browser-sync', ['sass'], function() {
     bs.init({
         server: {
-            baseDir: "./"
+            baseDir: "./dist"
         }
     });
 });
@@ -16,26 +18,33 @@ var autoprefixerOptions = {
   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
 };
 
-var input = './resources/scss/**/*.scss';
-var output = './resources/css';
-
 var sassOptions = {
   errLogToConsole: true,
   outputStyle: 'expanded'
 };
 
 gulp.task('sass', function () {
-    return gulp.src(input)
+    return gulp.src('./src/scss/**/*.scss')
                 .pipe(sourcemaps.init())
                 .pipe(sass(sassOptions).on('error', sass.logError))
                 .pipe(autoprefixer(autoprefixerOptions))
                 .pipe(sourcemaps.write())
-                .pipe(gulp.dest(output))
+                .pipe(gulp.dest('./dist/css'))
                 .pipe(bs.reload({stream: true}));
 });
 
+gulp.task('js', function(){
+    return gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+});
+gulp.task('html', function() {
+    return gulp.src('src/index.html')
+    .pipe(validator())
+    .pipe(gulp.dest('dist/'));
+  });
 gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch("./resources/scss/**/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', bs.reload);
-    gulp.watch("./resources/js/*.js").on('change', bs.reload);
+    gulp.watch("./src/scss/**/*.scss", ['sass']);
+    gulp.watch("./src/*.html",['html']).on('change', bs.reload);
+    gulp.watch("./src/js/*.js").on('change', bs.reload);
 });
--- resources/js/main.js
+++
@@ -1,5 +0,0 @@
-// I am an awesome guy
-
-var i = 0;
-i = 10;
-console.log(i);
